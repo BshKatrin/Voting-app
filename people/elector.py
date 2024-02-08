33c7b51ea268b78403839f77cee9cb1b6be2734b
De La Taille Lolainville Hadrien <hadrien.de_la_taille@etu.sorbonne-universite.fr>
2024-02-08 13:46:31 +0000

    Update elector.py

--- people/elector.py
+++ people/elector.py
@@ -1,23 +1,33 @@
+from dataclasses import dataclass, field, InitVar
+from typing import List
 from math import sqrt
 
 from .person import Person
+from .candidate import Candidate
 
 
+@dataclass(kw_only=True)
 class Elector(Person):
-    def __init__(self, position, candidates, candidate_ranking=[]):
-        super().__init__(position)
-        self.position = position
-        if candidate_ranking == []:
-            candidate_ranking = self.pos_to_rank(candidates)
-        self.candidate_ranking = candidate_ranking
+    candidates: InitVar[List[Candidate]]
+    candidates_ranked: List[Candidate] = field(default_factory=list, repr=False)
 
-    def dist_from_cand(self, list_candidates):
+    def __post_init__(self, candidates):
+        if not self.candidates_ranked:
+            self.candidates_ranked = self.pos_to_rank(candidates)
+
+    def dist_from_cand(self, candidates):
         distances = []
         x, y = self.position
-        for candidate in list_candidates:
-            x_c, y_c = candidate.getPosition()
+        for candidate in candidates:
+            x_c, y_c = candidate.get_position()
             distances.append(sqrt((x_c - x) ** 2 + (y_c - y) ** 2))
         return distances
+
+    def dist_from_one_cand(self,candidate):
+        x,y=self.position
+        xc,yc=candidate.get_position()
+        return (sqrt((xc-x)**2+ (yc-y)**2)) 
+    
 
     # Calculate ranking from given position (x,y)
     def pos_to_rank(self, candidates):
@@ -26,3 +36,8 @@ class Elector(Person):
             c for (c, _) in sorted(zip(candidates, distances), key=lambda t: t[1])
         ]
         return ranking
+
+    # Retourne une liste des electeurs genere de maniere aleatoire
+    @classmethod
+    def gen_rand_electors(cls, n, candidates):
+        return [Elector(candidates=candidates) for _ in range(n)]
