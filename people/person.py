aa73e9a5ae545aed88c263af746e0eb5a552db8f
De La Taille Lolainville Hadrien <hadrien.de_la_taille@etu.sorbonne-universite.fr>
2024-02-08 13:48:59 +0000

    Update person.py

--- people/person.py
+++ people/person.py
@@ -1,43 +1,22 @@
-from dataclasses import dataclass, field, InitVar
-from typing import List
-from math import sqrt
-
-from .person import Person
-from .candidate import Candidate
+from dataclasses import dataclass, field
+from itertools import count
+from random import uniform
 
 
-@dataclass(kw_only=True)
-class Elector(Person):
-    candidates: InitVar[List[Candidate]]
-    candidates_ranked: List[Candidate] = field(default_factory=list, repr=False)
+@dataclass(kw_only=True, eq=True)
+class Person:
+    def gen_rand_position():
+        return (uniform(-1, 1), uniform(-1, 1))
 
-    def __post_init__(self, candidates):
-        if not self.candidates_ranked:
-            self.candidates_ranked = self.pos_to_rank(candidates)
+    id: int = field(default_factory=count().__next__, compare=False)
+    position: (float, float) = field(default_factory=gen_rand_position, compare=False)
 
-    def dist_from_cand(self, candidates):
-        distances = []
+    def get_position(self):
+        return self.position
+
+    def __str__(self):
         x, y = self.position
-        for candidate in candidates:
-            x_c, y_c = candidate.get_position()
-            distances.append(sqrt((x_c - x) ** 2 + (y_c - y) ** 2))
-        return distances
+        return f"{self.__class__.__name__}({self.id}, ({x:.2f}, {y:.2f}))"
 
-    def dist_from_one_cand(self,candidate):
-        x,y=self.position
-        xc,yc=candidate.get_position()
-        return (sqrt((xc-x)**2+ (yc-y)**2)) 
-    
-
-    # Calculate ranking from given position (x,y)
-    def pos_to_rank(self, candidates):
-        distances = self.dist_from_cand(candidates)
-        ranking = [
-            c for (c, _) in sorted(zip(candidates, distances), key=lambda t: t[1])
-        ]
-        return ranking
-
-    # Retourne une liste des electeurs genere de maniere aleatoire
-    @classmethod
-    def gen_rand_electors(cls, n, candidates):
-        return [Elector(candidates=candidates) for _ in range(n)]
+    def __repr__(self):
+        return self.__str__()
