47ad33c8b31eebd8d29b5bcfd35eb67cad3562e6
BshKatrin <dyanko.ekaterina2015@gmail.com>
2024-04-01 10:40:30 +0200

    Revert "Update widget_map.py"
    This reverts commit 189846c2d6e4a42eec821acde5893de1d6eace84.

--- graphics/widget_map.py
+++ graphics/widget_map.py
@@ -6,8 +6,6 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QLineEdit,
     QSizePolicy,
-    QFileDialog,
-    QInputDialog
 )
 from PySide6.QtCore import Qt, Signal, Slot, QPoint
 from electoral_systems import Election
