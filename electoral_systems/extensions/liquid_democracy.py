fdf747643471ed623bd4505b0821ef6b061721b9
BshKatrin <dyanko.ekaterina2015@gmail.com>
2024-04-09 20:59:38 +0200

    liquid democracy docs

--- electoral_systems/extensions/liquid_democracy.py
+++ electoral_systems/extensions/liquid_democracy.py
@@ -1,11 +1,31 @@
-from random import choices
 from math import sqrt
+from random import choices
+from typing import List
+
+from people import Elector
 
 
 class LiquidDemocracy:
-    DELEGATION_GAP_COEF = 0.1  # Liquid democracy constant
+    """Une classe qui contient la fonctionnalité nécessaire pour la démocratie liquide, i.e. faire les délégations."""
 
-    def choose_delegee(electors):
+    DELEGATION_GAP_COEF = 0.1
+    """Une constante qui définit un rayon maximale d'une cercle pour choisir les délégataires.
+        Les délégataires doivent être assez proches par une position politique.
+    """
+
+    def choose_delegee(electors: List[Elector]) -> Elector:
+        """Choisir un électeur-délégataire parmi les électeurs de façon suivante:
+            - Calculer le taux de connaissances total des électeurs
+            - Choisir un électeur de manière aléatoire selon leur taux de connaissances.
+            Plus le taux de connaisances d'un électeur est élevé, plus il est probable qu'il soit choisi
+            comme un délégataire, et inversement, moins son taux de connaissances est élevé, moins il est probable
+            qu'il soit choisi comme un délégataire.
+
+        Args:
+            electors (List[Elector]) -> Une liste des électeurs qui peuvent être les délégataires.
+        Returns:
+            people.elector.Elector: un délégateur choisi.
+        """
         if len(electors) == 0:
             return None
 
@@ -25,14 +45,25 @@ class LiquidDemocracy:
         # Retourne un électeur à qui délégé
         return choices(electors, weights=weights, k=1)[0]
 
-    def choose_possible_delegees(electors, delegator):
+    def choose_possible_delegees(electors: List[Elector], delegator: Elector) -> List[Elector]:
+        """Choisir parmi `electors` des électeurs-délégataires pour un électeur `delegator` de façon suivante:
+            - Calculer la distance entre un électeur et `delegator`
+            - Si elle est inférieure à `DELEGATION_GAP_COEF`, ajouter cet électeur dans une liste des délégataires. Sinon, continuer.
+
+        Args:
+            electors (List[Elector]): tous les électeurs participant dans une élection
+            delagator (Elector): un électeur qui veut déléger
+
+        Returns:
+            List[Elector]: Une liste des électeurs qui peuvent être considérés comme des délégataires.
+        """
         electors_in_radius = []
-        x1, y1 = delegator.get_position()
+        x1, y1 = delegator.position
 
         for elector in electors:
             if elector == delegator:
                 continue
-            x2, y2 = elector.get_position()
+            x2, y2 = elector.position
             dist = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
             if dist <= LiquidDemocracy.DELEGATION_GAP_COEF and elector.weight != 0:
