014caab0e55473bd80c93e251b8f8b4c10af123f
Zerdoumi Haroun <haroun.zerdoumi@etu.sorbonne-universite.fr>
2024-04-13 08:30:54 +0000

    Replace plurality.py

--- electoral_systems/voting_rules/plurality.py
+++ electoral_systems/voting_rules/plurality.py
@@ -1,4 +1,4 @@
-"""Ce module fournit des fonctions nécessaires pour des règles du vote du type Pluralité"""
+"""Ce module fournit des fonctions nécessaires pour des règles de vote du type Pluralité"""
 
 from typing import List, Optional
 from .constants import PLURALITY_SIMPLE, PLURALITY_2_ROUNDS
@@ -10,21 +10,22 @@ from people import Candidate, Elector
 
 def apply_plurality_simple(electors: List[Elector], candidates: List[Candidate],
                            duels: Optional[duels_type] = None) -> List[Candidate]:
-    """Appliquer une règle du vote *Pluralité à 1 tour*. Possible d'appliquer cette règle du vote s'il existe au moins 2 candidats.
-    Principe d'une règle du vote *Pluralité à 1 tour*: 
+    """Applique la règle de vote *Pluralité à 1 tour*. Il n'est possible d'appliquer cette règle de vote que s'il existe au 
+    moins 2 candidats.
+    Principe d'une règle de vote *Pluralité à 1 tour*: 
         - Chaque électeur doit placer tous les candidats selon ses préférences dans l'ordre décroissant.
-        - Chaque candidat classé premier un point est attribué à son score. Les scores des autres candidats ne changent pas.
+        - Chaque candidat classé premier reçoit un point. Les scores des autres candidats ne changent pas.
 
 
     Args:
-        electors (List[people.elector.Elector]): Une liste de tous les électeurs participant dans une élection.
+        electors (List[people.elector.Elector]): Une liste de tous les électeurs participant à une élection.
             Leur liste `candidates_ranked` doit être remplie.
-        candidates (List[people.candidate.Candidate]): Une liste de tous les candidats qui participent dans une élection.
+        candidates (List[people.candidate.Candidate]): Une liste de tous les candidats qui participent à une élection.
         duels (Utls.duels_type): Un dictionnaire qui associe à chaque duel des candidats (gagnant, perdant) le nombre des fois
         que le candidat-gagnant a battu le candidat-perdant. Default = `None`.
     Returns:
         List[people.candidate.Candidate]:  Une liste des candidats triés dans l'ordre décroissant selon leurs scores dans
-            la règle du vote *Pluralité à 1 tour*
+            la règle de vote *Pluralité à 1 tour*
     """
     init_scores(candidates, PLURALITY_SIMPLE, 0)
     for elector in electors:
@@ -34,8 +35,9 @@ def apply_plurality_simple(electors: List[Elector], candidates: List[Candidate],
 
 
 def apply_plurality_rounds(electors: List[Elector], candidates: List[Candidate]) -> List[List[Candidate]]:
-    """Appliquer une règle du vote *Pluralité à 2 tours*. Possible d'appliquer cette règle du vote s'il existe au moins 3 candidats.
-      Principe d'une règle du vote *Pluralité à 2 tours*: 
+    """Applique une règle de vote *Pluralité à 2 tours*. Il n'est possible d'appliquer cette règle du vote que s'il existe 
+    au moins 3 candidats.
+      Principe d'une règle de vote *Pluralité à 2 tours*: 
         - Chaque électeur doit placer tous les candidats selon ses préférences dans l'ordre décroissant.
         - Tour 1 : Chaque candidat classé premier reçoit un point, tandis que les scores des autres candidats restent inchangés.
         - Tour 2: Ce tour commence si aucun candidat n'obtient la majorité absolue des votes au premier tour.
@@ -45,9 +47,9 @@ def apply_plurality_rounds(electors: List[Elector], candidates: List[Candidate])
 
 
     Args:
-        electors (List[people.elector.Elector]): Une liste de tous les électeurs participant dans une élection.
+        electors (List[people.elector.Elector]): Une liste de tous les électeurs participant à une élection.
             Leur liste `candidates_ranked` doit être remplie.
-        candidates (List[people.candidate.Candidate]): Une liste de tous les candidats qui participent dans une élection.
+        candidates (List[people.candidate.Candidate]): Une liste de tous les candidats qui participent à une élection.
 
     Returns:
         List[people.candidate.Candidate]: Une liste des listes (classement dans l'ordre décroissant) des candidats par tour. La longueur de la liste correpond
