6ee14dd41c959ebd21e1e2b8676f3b0d79d3e3ec
Zerdoumi Haroun <haroun.zerdoumi@etu.sorbonne-universite.fr>
2024-04-13 08:30:13 +0000

    Replace approval.py

--- electoral_systems/voting_rules/approval.py
+++ electoral_systems/voting_rules/approval.py
@@ -8,24 +8,23 @@ from people import Candidate, Elector
 
 def apply_approval(electors: List[Elector], candidates: List[Candidate],
                    gap: float, duels: Optional[duels_type] = None) -> List[Candidate]:
-    """Appliquer une règle du vote *Approbation*. Possible d'appliquer cette règle du vote s'il existe au moins 2 candidats.
-    Principe d'une règle du vote *Approbation*:  
-        - chaque électeur doit placer tous les candidats selon ses préférences dans l'ordre décroissant.
-        - Chaque candidat reçoit un score en fonction de sa position dans le classement et en fonction d'un rayon d'une cercle 
-        d'approbation. Le candidat classé premier reçoit un point à son score. Les autres candidats
-        reçoivent un point ssi ils sont dand une cercle d'approbation d'un électeur. 
-        Une cercle d'approbation d'un électeur est une cercle centré à la position d'un électeur et du rayon `gap` + distance entre
-        la position du candidat classé premier et celle d'un électeur.
+    """Applique la règle de vote Approbation. Il n'est possible d'appliquer cette règle de vote s'il existe 
+    au moins 2 candidats. Principe d'une règle de vote Approbation:
+    - Chaque électeur doit placer tous les candidats selon ses préférences dans l'ordre décroissant. 
+    - Chaque candidat reçoit un score en fonction de sa position dans le classement et en fonction d'un rayon d'un cercle d'approbation. 
+    Le candidat classé premier reçoit un point à son score. Les autres candidats reçoivent un point ssi ils sont dans une cercle 
+    d'approbation d'un électeur. Un cercle d'approbation d'un électeur est un cercle centré à la position d'un électeur et de rayon 
+    gap + distance entre la position du candidat classé premier et celle d'un électeur.
 
     Args:
-        electors (List[people.elector.Elector]): Une liste de tous les électeurs participant dans une élection.
+        electors (List[people.elector.Elector]): Une liste de tous les électeurs participant à une élection.
             Leur liste `candidates_ranked` doit être remplie.
-        candidates (List[people.candidate.Candidate]): Une liste de tous les candidats qui participent dans une élection.
-        duels (Utls.duels_type): Un dictionnaire qui associe à chaque duel des candidats (gagnant, perdant) le nombre des fois
+        candidates (List[people.candidate.Candidate]): Une liste de tous les candidats qui participent à une élection.
+        duels (Utls.duels_type): Un dictionnaire qui associe à chaque duel des candidats (gagnant, perdant) le nombre de fois
         que le candidat-gagnant a battu le candidat-perdant. Default = `None`.
     Returns:
-        List[people.candidate.Candidate]: Une liste des candidats triés dans l'ordre décroissant selon leurs dans
-            la règle du vote *Approbation*.
+        List[people.candidate.Candidate]: Une liste des candidats triés dans l'ordre décroissant selon leurs score selon
+            la règle de vote *Approbation*.
     """
     init_scores(candidates, APPROVAL, 0)
     for elector in electors:
