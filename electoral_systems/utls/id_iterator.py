144b2a65faa04a10f14be8a0b40d3811836eb2be
BshKatrin <dyanko.ekaterina2015@gmail.com>
2024-04-22 09:15:04 +0200

    docs changes integrated

--- electoral_systems/utls/id_iterator.py
+++ electoral_systems/utls/id_iterator.py
@@ -1,7 +1,13 @@
 class IdIterator:
-    """Un itérateur pour générer les IDs des candidats et des électeurs."""
+    """Un itérateur pour générer les IDs des candidats et des électeurs"""
 
-    def __init__(self, start):
+    def __init__(self, start:int):
+        """Un itérateur commence à partir du `start`.
+        
+        Args:
+            start (int): Un nombre à partir duquel il faut commencer la génération.
+        """
+
         self.start = start  # Premier nombre à générer
         self.current = self.start
 
@@ -14,5 +20,5 @@ class IdIterator:
         return id
 
     def restart(self):
-        """Recommence une génération, i.e. le nombre suivant généré sera `self.start`"""
+        """Recommence une génération des nombres à partir du `start`"""
         self.current = self.start
